17457e1200e1a25f3060098c4c53703b5101a5e6
Jsturgi4 <105025243+Jsturgi4@users.noreply.github.com>
2022-07-12 06:01:44 -0400

    added prework files to repository

---
+++ Prework Tip Calculator/Prework Tip Calculator/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Prework Tip Calculator
+//
+//  Created by jessica sturgis on 7/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+}
+
+    @IBAction func calculatorTip(_ sender: Any) {
+     
+        
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill *
+        tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
+}
